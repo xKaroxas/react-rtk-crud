d1942a553431d6ef3ecb4e0c5753ea3fcb67c771
karoxas <karoxas20@gmail.com>
2024-04-22 14:02:17 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import TaskForm from "./components/TaskForm";
+import TasksList from "./components/TasksList";
+
+function App() {
+  return (
+    <div className="bg-zinc-900 h-screen text-white">
+      <div className="flex items-center justify-center h-full">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<TasksList />} />
+            <Route path="/create-task" element={<TaskForm />} />
+            <Route path="/edit-task/:id" element={<TaskForm />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import tasksReducer from "../features/tasks/taskSlice";
+
+export const store = configureStore({
+  reducer: { tasks: tasksReducer },
+});
